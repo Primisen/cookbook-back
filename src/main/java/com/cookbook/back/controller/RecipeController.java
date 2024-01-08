9b4af314482c8806116b8f5db7600e8f7f8f32c2
Nadzeya <primisen17@gmail.com>
2024-01-08 16:07:14 +0100

    Created recipe controller

---
+++ src/main/java/com/cookbook/back/controller/RecipeController.java
@@ -0,0 +1,24 @@
+package com.cookbook.back.controller;
+
+import com.cookbook.back.domain.Recipe;
+import com.cookbook.back.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    @Autowired
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping("/recipes")
+    public Iterable<Recipe> getRecipes(Model model) {
+        return recipeService.findAll();
+    }
+}
